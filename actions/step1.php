ae3e7dde559daea863d5076e25b4ec13f5e2218e
Michal <a225.waldek@gmail.com>
2015-05-03 15:56:25 +1000

    Dodana informacja o publicznym kodzie źródłowym

--- actions/step1.php
+++ actions/step1.php
@@ -43,7 +43,9 @@
                                 </div>
 
                                 <div id="emailFormIcon"></div>
-<label style="font-size:12px; color: grey;">* Hasła nigdy nie widzimy, po pobraniu kontaktów od razu zostaje usunięte z systemu i zapomniane.</label>
+                                    <label style="font-size:12px; color: grey;">* Hasło nie zostaje zapisane, a po pobraniu kontaktów jest natychmiast usunięte z systemu i zapomniane.<br />
+                                    Jako dowód, udostępniamy publicznie <a href="https://github.com/MichalKot89/popieramkukiza.pl" target="_blank">kod źródłowy strony</a>.
+                                    </label>
                                 <button type="submit" class="btn btn-default btn pull-right">Wybierz kontakty</button>
 
                                 <input type="hidden" name="provider" id="emailProvider" />
